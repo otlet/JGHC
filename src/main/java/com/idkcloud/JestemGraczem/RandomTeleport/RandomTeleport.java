4bc729e58713f3b185b403e5a06dd0a4c7467d82
Otlet <otlet@protonmail.com>
2019-10-09 13:41:59 +0200

    RTP działą poprawnie, ale brakuje cooldown

--- src/main/java/com/idkcloud/JestemGraczem/RandomTeleport/RandomTeleport.java
+++ src/main/java/com/idkcloud/JestemGraczem/RandomTeleport/RandomTeleport.java
@@ -8,58 +8,78 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 public class RandomTeleport implements CommandExecutor {
     private JestemGraczem main = JestemGraczem.getPlugin(JestemGraczem.class);
+    private int cooldown;
+    private HashMap<String, Long> cooldowns = new HashMap<>();
 
     public RandomTeleport() {
         this.generateDefaultConfiguration();
+        cooldown = main.getConfig().getInt("RandomTeleport.Cooldown");
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        // Sprawdzenie, czy RTP jest włączone w konfiguracji
         if (!main.getConfig().getBoolean("RandomTeleport.Activate")) {
             String message = "Random Teleport jest wyłączony!";
             sender.sendMessage(message);
-            return true;
+            return false;
         }
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            String world = main.getConfig().getString("RandomTeleport.Location.World");
-            if (player.getWorld().getName().equals(world)) {
 
-                int minX = main.getConfig().getInt("RandomTeleport.Location.MinX");
-                int minZ = main.getConfig().getInt("RandomTeleport.Location.MinZ");
-                int maxX = main.getConfig().getInt("RandomTeleport.Location.MaxX");
-                int maxZ = main.getConfig().getInt("RandomTeleport.Location.MaxZ");
-                List<String> avoid = main.getConfig().getStringList("RandomTeleport.Avoid");
+        //Weryfikacje, czy wysyłający komendę jest gracz
+        if (!(sender instanceof Player))
+            return false;
 
-                Random r = new Random();
-                int X = r.nextInt(maxX - minX) + minX;
-                int Y = 255;
-                int Z = r.nextInt(maxZ - minZ) + minZ;
+        Player player = (Player) sender;
 
-                main.getLogger().info(String.format("X: %s | Y: %s | Z: %s", X, Y, Z));
-
-                do {
-                    Location location = new Location(player.getWorld(), X, Y, Z);
-                    if (!avoid.contains(location.getBlock().getType().name()))
-                        break;
-                    Y--;
-                } while (Y > 0);
-
-                Location location = new Location(player.getWorld(), X, Y, Z);
-                player.teleport(location);
-
-            } else {
-                player.sendMessage("Działa tylko na mapie: " + world);
+        //Czy gracz już jest w pamięci cooldowna
+        if (cooldowns.containsKey(player.getName())) {
+            //Jeśli tak, to czy jego cooldown już minął.
+            if (cooldowns.get(player.getName()) - System.currentTimeMillis() * 1000 > cooldown) {
+                sender.sendMessage("Nie tak szybko!");
+                return false;
             }
-        } else {
-            sender.sendMessage("Komenda tylko do wykonania jako gracz!");
         }
-        return true;
+
+        // Weryfikacja, czy gracz jest na odpowiedniej mapie
+        String world = main.getConfig().getString("RandomTeleport.Location.World");
+        if (!player.getWorld().getName().equals(world))
+            player.sendMessage("Tylko można na mapie: " + world);
+
+        // Zapisywanie konfiguracji do osobnej zmiennej
+        int minX = main.getConfig().getInt("RandomTeleport.Location.MinX");
+        int minZ = main.getConfig().getInt("RandomTeleport.Location.MinZ");
+        int maxX = main.getConfig().getInt("RandomTeleport.Location.MaxX");
+        int maxZ = main.getConfig().getInt("RandomTeleport.Location.MaxZ");
+        List<String> avoid = main.getConfig().getStringList("RandomTeleport.Avoid");
+
+        // Generowanie randomowego miejsca
+        Random r = new Random();
+        int X = r.nextInt(maxX - minX) + minX;      // Szerokość
+        int Y = 255;                                       // Wysokość
+        int Z = r.nextInt(maxZ - minZ) + minZ;     // Długość
+
+        main.getLogger().info(String.format("X: %s | Y: %s | Z: %s", X, Y, Z));
+
+        // Wykonuj dopóki Y > 0 (czyli wysokość)
+        do {
+            Location location = new Location(player.getWorld(), X, Y, Z);
+            // Jeśli blok nie posiada zakazanego typu (np.: woda, czy lava) to teleportuj go + zapis cooldown
+            if (!avoid.contains(location.getBlock().getType().name())) {
+                Location tpLocation = new Location(player.getWorld(), X, Y + 1, Z);
+                cooldowns.put(player.getName(), System.currentTimeMillis() * 1000);
+                player.teleport(tpLocation);
+                return true;
+            }
+            Y--;
+        } while (Y > 0);
+
+        return false;
     }
 
     private String[] getDefaultAvoidMaterial() {
